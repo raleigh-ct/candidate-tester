a70445346471b3b7c9af0fe1152d6fc0dac6a1e7
Raleigh Moore <raleighcmoore@gmail.com>
2021-01-25 21:49:39 +0000

    Updated question variables to arrays

--- index.js
+++ index.js
@@ -9,15 +9,19 @@
 const input = require('readline-sync');
 
 let candidateName = input.question("Please Enter Your Name: ");
-// console.log(candidateName);
-let quizQuestion1 = "True or false: 5000 meters = 5 kilometers. ";
-let quizAnswer1 = 'true';
-let candidateAnswer1 = input.question(quizQuestion1);
+
+let questions = ["True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', "Who was the first American woman in space? ", "What is the minimum crew size for the International Space Station (ISS)? "];
+
+let answers = ["true", "40", "trajectory", "sally ride", "3"]
+
+let candidateAnswer1 = input.question(questions[3]);
 let lowerAnswer1 = candidateAnswer1.toLowerCase();
 // console.log(candidateAnswer1);
 
-if (quizAnswer1 === lowerAnswer1) {
-  console.log("Question 1: Correctly Answered by " + candidateName)
+
+if (answers[3] === lowerAnswer1) {
+  console.log(`Question 1: Correctly Answered by ${candidateName}`)
 } else {
-  console.log("Question 1: Answered Incorrectly by " + candidateName)
+  console.log(`Question 1: Answered Incorrectly by ${candidateName}`)
 }
+
