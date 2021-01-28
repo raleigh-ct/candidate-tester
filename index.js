03b2aa1cc8a4ecbb90b7788eac8ff78722b24699
Raleigh Moore <raleighcmoore@gmail.com>
2021-01-28 00:48:19 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -12,16 +12,60 @@ let candidateName = input.question("Please Enter Your Name: ");
 
 let questions = ["True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", 'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ', "Who was the first American woman in space? ", "What is the minimum crew size for the International Space Station (ISS)? "];
 
-let answers = ["true", "40", "trajectory", "sally ride", "3"]
+let answers = ["true", "40", "trajectory", "sally ride", "3"];
 
-let candidateAnswer1 = input.question(questions[3]);
-let lowerAnswer1 = candidateAnswer1.toLowerCase();
-// console.log(candidateAnswer1);
+let correctAnswers = 0;
+let questionCount = 5;
 
+let candidateAnswers = [];
 
-if (answers[3] === lowerAnswer1) {
-  console.log(`Question 1: Correctly Answered by ${candidateName}`)
+let percentageGrade = 0;
+let status = '';
+
+for (let i=0; i < questions.length; i++) {
+candidateAnswers.push(input.question(questions[i]).toLowerCase());
+
+if (answers[i] === candidateAnswers[i]) {
+  console.log(`Correctly Answered by ${candidateName}`)
+  correctAnswers += 1;
 } else {
-  console.log(`Question 1: Answered Incorrectly by ${candidateName}`)
+  console.log(`Answered Incorrectly by ${candidateName}`)
+}
 }
 
+percentageGrade = (correctAnswers/questionCount) * 100;
+
+if (percentageGrade >= 80) {
+  status = "PASSED" 
+} else {
+  status = "FAILED"
+}
+
+
+
+
+console.log(`
+Candidate Name: ${candidateName}
+1) ${questions[0]}
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: ${answers[0]}
+
+2) ${questions[1]}
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: ${answers[1]}
+
+3) ${questions[2]}
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: ${answers[2]}
+
+4) ${questions[3]}
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: ${answers[3]}
+
+5) ${questions[4]}
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: ${answers[4]}
+
+>>> Overall Grade: ${percentageGrade}% (${correctAnswers} of ${questionCount} responses correct) <<<
+>>> ${status} <<<
+`)
